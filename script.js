2081cc347c0c0d5bd9e5f6938a62128c7e21f04f
marcdtnguyen <86821559+marcdtnguyen@users.noreply.github.com>
2022-11-18 10:26:30 +1100

    Complete rock-paper-scissors

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
-function getComputerChoice(){
-
-};
-
-function playRound(playerSelection, computerSelection){
-
-};
+const options = ['rock', 'paper', 'scissors'];
 
 function game(){
-    playRound();
+    const playerSelection = prompt('Rock || Paper || Scissors:', '').toLowerCase();
+    const computerSelection = getComputerChoice();
+    if(checkInput(playerSelection)){
+        alert(`Human throws ${playerSelection}!`);
+        alert(`Computer throws ${computerSelection}!`)
+        print(playRound(playerSelection, computerSelection));
+    } else {
+        alert("Invalid input: Please choose rock, paper or scissors.")
+    }
 }
+
+function getComputerChoice(){
+    return options[getRandomNumber()];
+};
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 3);
+}
+
+function print(results){
+    switch(results){
+        case 'tie':
+            alert('It\'s a tie.');
+            break;
+        case 'computer':
+            alert('Computer wins!');
+            break;
+        case 'human':
+            alert('Human wins!');
+            break;
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return 'tie';
+    } 
+
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'paper') 
+            return 'computer';
+        if(computerSelection === 'scissors')
+            return 'human';
+    }
+
+    if(playerSelection === 'paper'){
+        if(computerSelection === 'scissors')
+            return 'computer';
+        if(computerSelection === 'rock')
+            return 'human';
+    }
+
+    if(playerSelection === 'scissors'){
+        if(computerSelection === 'rock')
+            return 'computer';
+        if(computerSelection === 'paper')
+            return 'human';
+    }
+
+};
+
+function checkInput(playerSelection){
+    return options.includes(playerSelection);
+}
+
+game();
